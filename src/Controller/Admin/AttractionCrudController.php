d21c9cf30815b08f38013a92a58a3f425ea72e59
antonin <antonin.val@gmail.com>
2021-04-06 13:44:38 +0200

    attraction entity ok and easy admin

---
+++ src/Controller/Admin/AttractionCrudController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Attraction;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+
+class AttractionCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Attraction::class;
+    }
+
+    
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            TextField::new('name'),
+            SlugField::new('slug')->setTargetFieldName('name'),
+            ImageField::new('picture')
+                ->setBasePath('uploads/')
+                ->setUploadDir('public/uploads/')
+                ->setUploadedFileNamePattern('[randomhash].[extension]')
+                ->setRequired(false),
+            TextField::new('subtitle'),
+            TextareaField::new('description'),
+            BooleanField::new('isBest')
+
+            
+        ];
+    }
+    
+}
---
+++ src/Controller/AttractionController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Attraction;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AttractionController extends AbstractController
+{
+
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+
+    /**
+     * @Route("/attraction", name="attraction")
+     */
+    public function index(): Response
+    {
+
+        $attractions = $this->entityManager->getRepository(Attraction::class)->findAll();
+        
+
+        return $this->render('attraction/index.html.twig', [
+            'attractions' => $attractions,
+        ]);
+    }
+
+    /**
+     * @Route("/attraction/{slug}", name="attraction_detail")
+     */
+    public function show($slug): Response
+    {
+
+        $attraction = $this->entityManager->getRepository(Attraction::class)->findOneBy([
+            'slug' => $slug
+        ]);
+
+        $attractions = $this->entityManager->getRepository(Attraction::class)->findBy([
+            'isBest' => 1
+        ]);
+        
+
+        return $this->render('attraction/show.html.twig', [
+            'attractions' => $attractions,
+            'attraction' => $attraction
+        ]);
+    }
+}
