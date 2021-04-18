a0a60b769a21116da7a1c01ae4666a242a7fceed
antonin <antonin.val@gmail.com>
2021-04-18 17:20:08 +0200

    stripe success and cancel ok sauf twig

--- src/Controller/ReservationCancelController.php
+++ src/Controller/ReservationCancelController.php
@@ -2,19 +2,36 @@
 
 namespace App\Controller;
 
+use App\Entity\Reservation;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ReservationCancelController extends AbstractController
 {
-    /**
-     * @Route("/reservation/cancel", name="reservation_cancel")
-     */
-    public function index(): Response
+
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
     {
-        return $this->render('reservation_cancel/index.html.twig', [
-            'controller_name' => 'ReservationCancelController',
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * @Route("/reservation/erreur/{stripeId}", name="reservation_cancel")
+     */
+    public function index($stripeId): Response
+    {
+
+        $reservation = $this->entityManager->getRepository(Reservation::class)->findOneBy([
+            'stripesession' => $stripeId
         ]);
+
+        if (!$reservation || $reservation->getUser() != $this->getUser()) {
+            return $this->redirectToRoute('home');
+        }
+
+        return $this->render('reservation_cancel/index.html.twig');
     }
 }
